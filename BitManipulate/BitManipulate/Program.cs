110f2ba74a2c987cea5bdeb0b42d40deedc0ba03
Intan Permatasari <Intan Permatasari@DESKTOP-N1FKJCQ>
2017-06-08 09:53:19 +0700

    first

---
+++ BitManipulate/BitManipulate/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitManipulate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(10 ^ 10);
+            /*int n = 1234;
+            string b = Convert.ToString(n,2);
+            var biner = b.ToCharArray();
+            int count = 0;
+
+            foreach (var a in biner)
+            {
+                if (a == '1')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);*/
+
+            string num1 = "10";
+            string num2 = "0";
+
+            char[] des1 = num1.ToCharArray();
+
+            List<int> des1a = new List<int>();
+            for (int i=0;i<des1.Length;i++)
+            {
+                if(des1[i] == '1')
+                {
+                    des1a.Add(1);
+                }else if(des1[i] == '0')
+                {
+                    des1a.Add(0);
+                }
+            }
+            double jum1=0;
+            foreach(var a in des1a)
+                 Console.Write(a);
+
+            int k = 0;
+            for(int i = des1a.Count()-1; i >=0 ; i--)
+            {
+                jum1 = jum1 + (Math.Pow(2, k) * des1a[i] );
+                k++;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(jum1);
+
+            //--
+            char[] des2 = num2.ToCharArray();
+
+            List<int> des2a = new List<int>();
+            for (int i = 0; i < des2.Length; i++)
+            {
+                if (des2[i] == '1')
+                {
+                    des2a.Add(1);
+                }
+                else if (des2[i] == '0')
+                {
+                    des2a.Add(0);
+                }
+            }
+            double jum2 = 0;
+            foreach (var a in des2a)
+                Console.Write(a);
+
+            int j = 0;
+            for (int i = des2a.Count() - 1; i >= 0; i--)
+            {
+                jum2 = jum2 + (Math.Pow(2, j) * des2a[i]);
+                j++;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(jum2);
+
+            int total = (int)(jum1) + (int)(jum2);
+            Console.WriteLine(total);
+
+
+
+
+        }
+    }
+}
--- LatihanAlpro2/LatihanAlpro2/Program.cs
+++ LatihanAlpro2/LatihanAlpro2/Program.cs
@@ -10,11 +10,24 @@ namespace LatihanAlpro2
     {
         static void Main(string[] args)
         {
-            int n = 5;
-            Console.WriteLine(factorial(n));
+            //ulong n = 65;
+            //Console.WriteLine(factorial(n));
+            /*
+            int m = 5;
+            for(int i = 0; i < m; i++)
+            {
+                Console.Write(fibbonaci(i)+" ");
+            }
+            Console.WriteLine();
+            */
+           // fib2(1, 1, 0, 5);
+
+            int[] arr = { 1, 3, 4, 7 };
+            Console.WriteLine(jumArr(arr, 0));
+            
         }
 
-        public static int factorial(int n)
+        public static ulong factorial(ulong n)
         {
             if (n == 1)
             {
@@ -24,6 +37,41 @@ namespace LatihanAlpro2
             {
                 return n * factorial(n - 1);
             }
+        }
+
+        public static int fibbonaci(int m)
+        {
+            if(m == 0 || m==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibbonaci(m - 1) + fibbonaci(m - 2);
+            }
+        }
+
+        public static void fib2(int a, int b, int counter, int len)
+        {
+            if (counter < len)
+            {
+                Console.Write(a+" ");
+                fib2(b, a + b, counter + 1, len);
+            }
+        }
+
+        public static int jumArr(int[] arr,int counter)
+        {
+            if(counter == arr.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                return arr[counter] + jumArr(arr, counter + 1);
+            }
+               
+              
         }
 
     }
